39168bac0ef952536f37a4c0ccdb993748abf60e
yippptay <yippptay@gmail.com>
2020-06-25 02:38:30 -0700

    ?

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// require("dotenv").config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = process.env.PORT;
