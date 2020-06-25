c959602e6142665053d2db9497162f8ad48bddaa
yippptay <yippptay@gmail.com>
2020-06-24 23:45:58 -0700

    override

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const methodOverride =    require("method-override");
 const mongoose = require("mongoose");
 const db = mongoose.connection;
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const routes = require("./controllers/routes.js");
+
+/****
+ * Error / Success
+ **/
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongo disconnected"));
+// open the connection to mongo
+db.on("open", () => {});
 
 /**
  * Middleware
